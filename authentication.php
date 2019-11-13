cfc3ae78fd1c9ac754cf58c287b21966d62225d5
cctl <cctl@ideapad>
2019-11-14 02:05:41 +0530

    redirect to error page on unauthorized access

--- authentication.php
+++ authentication.php
@@ -1,29 +1,41 @@
 <?php
+
 //Connect to the data base. PHP will check if the file has already been 
 //included, and if so, not include (require) it again
 require_once 'header/connection.php';
 //Start new or resume existing session
 session_start();
 
-//receive the post parameters of email and password
-$email = $_POST['email'];
-$password = $_POST['password'];
+//Check if POST parameters are set
+if (isset($_POST['email']) && isset($_POST['password'])) {
+    //receive the post parameters of email and password
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 //check if the credential matches entry in database
-$sql ="Select * from users where email='$email' and password = '$password' ";
+    $sql = "Select * from users where email='$email' and password = '$password' ";
 //Execute the query to the MySQL data base
-$result = mysqli_query($conn, $sql);
+    $result = mysqli_query($conn, $sql);
 //If exactly one row is fetched
-if ($result->num_rows == 1) {
-    //Set session "loggedIn" to TRUE
-    $_SESSION['loggedIn'] = true;
-    /* Redirect browser */
-    header('location: dashboard.php');
-    /* Make sure that code below does not get executed when we redirect. */
-    exit();
-} else {
-    $response->message =  "Incorrect credentials";
-    echo json_encode($response);
+    if ($result->num_rows == 1) {
+        //Set session "loggedIn" to TRUE
+        $_SESSION['loggedIn'] = true;
+        /* Redirect browser */
+        header('location: dashboard.php');
+        /* Make sure that code below does not get executed when we redirect. */
+        exit();
+    } else {
+        $response->message = "Incorrect credentials";
+        echo json_encode($response);
+    }
 }
+else{
+    /* Redirect browser */
+    header('Location:error.php');
+    /* Make sure that code below does not get executed when we redirect. */
+    exit; 
+}
+
+
 //Closes a previously opened database connection
 $conn->close();
 ?>
